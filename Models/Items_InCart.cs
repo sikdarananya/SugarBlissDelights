bf506c36f0bca8685119c33e55354b06c7b80c66
Ananya Sikdar <91484160+sikdarananya@users.noreply.github.com>
2024-05-29 19:23:31 +0530

    models

---
+++ Models/Items_InCart.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SweetShop_MVC.Models
+{
+    public class Items_InCart
+    {
+        public Item Product { get; set; }
+
+        public int Quantity { get; set; }
+    }
+}
